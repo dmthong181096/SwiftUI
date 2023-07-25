c2a1a9ad58f96d98b66f35765269eb20e60fc314
ThongDoan <122347323+ThongDoan96@users.noreply.github.com>
2023-07-25 07:27:46 +0700

    init project

---
+++ demoaa/demoaa/demoaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoaaApp.swift
+//  demoaa
+//
+//  Created by FE-Thong on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct demoaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
