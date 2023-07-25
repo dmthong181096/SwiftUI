0be3c3f2f7580d65e858fb0c1defa10901027a57
ThongDoan <122347323+ThongDoan96@users.noreply.github.com>
2023-07-25 21:32:54 +0700

    init project MuaHoVeSo

---
+++ MuaHoVeSo/MuaHoVeSo/MuaHoVeSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuaHoVeSoApp.swift
+//  MuaHoVeSo
+//
+//  Created by FE-Thong on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MuaHoVeSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
